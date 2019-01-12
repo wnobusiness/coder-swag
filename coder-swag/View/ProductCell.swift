3919956120c8bfc095d441a5df6618bb46292df8
Krishna Torrence <justnerd@Krishnas-MacBook-Pro.local>
2019-01-12 12:21:24 -0500

    add collection view controller, add image, label, price, and connected all items to the view controller

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Krishna Torrence on 1/12/19.
+//  Copyright © 2019 Krishna Torrence. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+}
