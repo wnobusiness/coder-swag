98d750bfdb477b55a2224023429a216f1478350c
Krishna Torrence <justnerd@Krishnas-MacBook-Pro.local>
2019-01-12 22:14:31 -0500

    Finish up coder-swag

--- coder-swag/View/ProductCell.swift
+++ coder-swag/View/ProductCell.swift
@@ -13,5 +13,9 @@ class ProductCell: UICollectionViewCell {
     @IBOutlet weak var productTitle: UILabel!
     @IBOutlet weak var productPrice: UILabel!
     
-    
+    func updateView(product: Product) {
+        productImage.image = UIImage(named: product.imageName)
+        productTitle.text = product.title
+        productPrice.text = product.price
+    }
 }
